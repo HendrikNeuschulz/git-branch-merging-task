22a3b36e28c5a1637aba097f582fe5eb0a7ba52a
Hendrik Neuschulz <hneuhb@t-online.de>
2023-02-22 09:12:10 +0100

    add cool new feature

--- src/FeatureA.java
+++ src/FeatureA.java
@@ -6,6 +6,6 @@ public class FeatureA {
 public class FeatureA{
 
     public static void main(String[] args) {
-        System.out.println("Hallo, das ist der Code von Hendrik");
+        System.out.println("Hallo, das ist der Code von  - Hendrik");
     }
 }
